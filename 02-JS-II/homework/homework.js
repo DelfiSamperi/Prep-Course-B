812ac02e8d56c71f86cb84c791e56e40bcd053b8
unknown <delfisamperi@gmail.com>
2022-10-13 16:27:12 -0300

    cambio1

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -7,12 +7,14 @@
 function devolverString(str) {
     // "Return" la string provista: str
     // Tu código:
+    return str;
   }
   
   function suma(x, y) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código:
+    x + y;
   }
   
   function resta(x, y) {
@@ -40,23 +42,37 @@ function devolverString(str) {
     // Devuelve "true" si las dos strings tienen la misma longitud
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (str1.length === str2.length) {
+    return true
+  } else return false
   }
   
   function menosQueNoventa(num) {
     // Devuelve "true" si el argumento de la función "num" es menor que noventa
     // De lo contrario, devuelve "false"
     // Tu código:
-  }
+      if (num <90) {
+        return true;
+      } 
+      else {
+        return false
+      }
+    }
   
   function mayorQueCincuenta(num) {
     // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (num >50) {
+      return true
+    }
+    else return false
   }
   
   function obtenerResto(x, y) {
     // Obten el resto de la división de "x" entre "y"
     // Tu código:
+    return x % y
   }
   
   function esPar(num) {
